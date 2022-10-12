7197ef45855d84a29f806b55eef26c4d9450c1ec
shahfaisal12 <faisalmkd02@gmail.com>
2022-10-12 22:12:06 +0500

    token section modefied add some animation

--- src/components/elements/TokenDistribution.jsx
+++ src/components/elements/TokenDistribution.jsx
@@ -7,28 +7,30 @@ const TokenDistribution = () => {
                 <div className="row">
                     <h1 className='text-center fw-bold'>Token <span className='text-danger'>Distribution</span></h1>
                 </div>
-                <div className="row">
-                    <div class="token-image position-realtive">
-                        <div class="token-sale__graph-ico text-center token-image-graph-info">
-                            <h5 class="fs-22"><strong>50%</strong></h5>
-                            <strong>Liquidity Rewards (Locked)</strong>
-                        </div>
-                        <div class="token-image-shadow"></div>
-                        <div class="text-center token-image-info">
-                            <h5 class="fs-22"><strong>10%</strong></h5>
-                            <span><strong>Growth Hacking</strong></span>
-                        </div>
-                        <div class="text-center token-image-info">
-                            <h5 class="fs-22"><strong>10%</strong>
-                            </h5><span><strong>Liquidity</strong></span>
-                        </div>
-                        <div class="text-center token-image-info">
-                            <h5 class="fs-22"><strong>10%</strong></h5>
-                            <span><strong>Team - locked</strong></span>
-                        </div>
-                        <div class="text-center token-image-info">
-                            <h5 class="fs-22"><strong>20%</strong></h5>
-                            <span><strong>Public Sale</strong></span>
+                <div className="row pt-5">
+                    <div className="col-md-6 m-auto py-5">
+                        <div class="token-image position-realtive">
+                            <div class="text-center token-image-graph-info" data-aos="fade-down" data-aos-duration="infinity">
+                                <h5 class="fs-22"><strong>50%</strong></h5>
+                                <strong>Liquidity Rewards (Locked)</strong>
+                            </div>
+                            <div class="token-image-shadow"></div>
+                            <div class="text-center token-image-info">
+                                <h5 class="fs-22"><strong>10%</strong></h5>
+                                <span><strong>Growth Hacking</strong></span>
+                            </div>
+                            <div class="text-center token-image-info">
+                                <h5 class="fs-22"><strong>10%</strong>
+                                </h5><span><strong>Liquidity</strong></span>
+                            </div>
+                            <div class="text-center token-image-info">
+                                <h5 class="fs-22"><strong>10%</strong></h5>
+                                <span><strong>Team - locked</strong></span>
+                            </div>
+                            <div class="text-center token-image-info">
+                                <h5 class="fs-22"><strong>20%</strong></h5>
+                                <span><strong>Public Sale</strong></span>
+                            </div>
                         </div>
                     </div>
                 </div>
